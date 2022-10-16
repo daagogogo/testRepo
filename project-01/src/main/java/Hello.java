d16a63166438ffaada8fcaded52c77cb0a792b41
daagogogo <58076079+daagogogo@users.noreply.github.com>
2022-10-16 17:12:46 +0800

    four commit

--- project-01/src/main/java/Hello.java
+++ project-01/src/main/java/Hello.java
@@ -1,7 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("git hello");
-        System.out.println("git hello1");
-        System.out.println("git hello2");
+        System.out.println("hello");
     }
 }
